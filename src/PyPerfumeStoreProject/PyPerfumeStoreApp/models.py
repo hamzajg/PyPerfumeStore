507fce9dbbb798ecc2b89c8831dae283a11ae99d
Hamza JGUERIM <hamzajg@gmail.com>
2018-01-18 13:31:28 +0100

    Add WebApp Resources...

--- src/PyPerfumeStoreProject/PyPerfumeStoreApp/models.py
+++ src/PyPerfumeStoreProject/PyPerfumeStoreApp/models.py
@@ -2,12 +2,40 @@ from django.db import models
 
 # Create your models here.
 
+class StoreInfo(models.Model):
+    storeInfoId = models.AutoField(primary_key=True)
+    storeName = models.CharField(max_length=50)
+
 class ProductType(models.Model):
     productTypeId = models.AutoField(primary_key=True)
     productTypeName = models.CharField(max_length=80)
-    
+
+class ProductCategory(models.Model):
+    productCategoryId = models.AutoField(primary_key=True)
+    productCategoryName = models.CharField(max_length=80)
+
 class Product(models.Model):
     productId = models.AutoField(primary_key=True)
     productCode = models.CharField(max_length=50)
     productName = models.CharField(max_length=80)
-    productTypeId = ForeignKey(model=ProductType)
+    productTypeId = models.ForeignKey(ProductType, on_delete=models.CASCADE)
+
+# class ProductPromo(models.Model):
+
+# class ProductBrand(models.Model):
+
+# class Customer(models.Model):
+
+# class Provider(models.Model):
+
+# class Offers(models.Model):
+
+# class Gift(models.Model):
+
+# class Sale(models.Model):
+
+# class User(models.Model):
+
+# class UserAccount(models.Model):
+
+# class WishList(models.Model):
