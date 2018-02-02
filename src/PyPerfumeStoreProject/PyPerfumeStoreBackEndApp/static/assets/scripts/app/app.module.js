fccce75fa5f28d99b29dce3140ff741eef33f4ee
hamzajg <hamzajg@gmail.com>
2018-02-03 08:13:32 +0900

    Work On BackEnd App..

---
+++ src/PyPerfumeStoreProject/PyPerfumeStoreBackEndApp/static/assets/scripts/app/app.module.js
@@ -0,0 +1,23 @@
+(function () {
+    'use strict';
+
+    angular.module('BackEndApp', [
+        // Core modules
+         'BackEndApp.core'
+
+        // Custom Feature modules
+        //,'BackEndApp.chart'
+        //,'BackEndApp.ui'
+        //,'BackEndApp.ui.form'
+        //,'BackEndApp.ui.form.validation'
+        //,'BackEndApp.page'
+        //,'BackEndApp.table'
+
+        // 3rd party feature modules
+        ,'mgo-angular-wizard'
+        ,'ui.tree'
+        ,'ngMap'
+        ,'textAngular'
+    ]);
+
+})();
